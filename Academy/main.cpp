5b95e58b6c9b4813a16b3b7cc8e33dd4772fc8c8
Виктор <@Laptop>
2024-06-23 02:33:20 +1000

    task completed

---
+++ Academy/main.cpp
@@ -0,0 +1,268 @@
+﻿#include<iostream>
+#include<string>
+
+using namespace std;
+
+#define delimiter "\n------------------------------------\n"
+
+#define HUMAN_TAKE_PARAMETERS	const std::string& last_name, const std::string& first_name, unsigned int age
+#define HUMAN_GIVE_PARAMETERS	last_name, first_name, age
+
+class Human
+{
+	std::string last_name;
+	std::string first_name;
+	unsigned int age;
+public:
+	const std::string& get_last_name()const
+	{
+		return last_name;
+	}
+	const std::string& get_first_name()const
+	{
+		return first_name;
+	}
+	const unsigned int get_age()const
+	{
+		return age;
+	}
+	void set_last_name(const std::string& last_name)
+	{
+		this->last_name = last_name;
+	}
+	void set_first_name(const std::string& first_name)
+	{
+		this->first_name = first_name;
+	}
+	void set_age(unsigned int age)
+	{
+		this->age = age;
+	}
+
+	//				Constructors:
+	Human(HUMAN_TAKE_PARAMETERS)
+	{
+		set_last_name(last_name);
+		set_first_name(first_name);
+		set_age(age);
+		cout << "HConstructor:\t" << this << endl;
+	}
+	~Human()
+	{
+		cout << "HDestructor:\t" << this << endl;
+	}
+
+	//				Methods:
+	virtual void info()const
+	{
+		cout << last_name << " " << first_name << " " << age << " y/o" << endl;
+	}
+};
+
+#define STUDENT_TAKE_PARAMETERS const std::string& speciality, const std::string& group, double rating, double attendance
+#define STUDENT_GIVE_PARAMETERS speciality, group, rating, attendance
+class Student :public Human
+{
+	std::string speciality;
+	std::string group;
+	double rating;
+	double attendance;
+public:
+	const std::string& get_speciality()const
+	{
+		return speciality;
+	}
+	const std::string& get_group()const
+	{
+		return group;
+	}
+	double get_rating()const
+	{
+		return rating;
+	}
+	double get_attendance()const
+	{
+		return attendance;
+	}
+	void set_speciality(const std::string& speciality)
+	{
+		this->speciality = speciality;
+	}
+	void set_group(const std::string& group)
+	{
+		this->group = group;
+	}
+	void set_rating(double rating)
+	{
+		this->rating = rating;
+	}
+	void set_attendance(double attendance)
+	{
+		this->attendance = attendance;
+	}
+
+	//				Constructors:
+	Student(HUMAN_TAKE_PARAMETERS, STUDENT_TAKE_PARAMETERS) :Human(HUMAN_GIVE_PARAMETERS)
+	{
+		set_speciality(speciality);
+		set_group(group);
+		set_rating(rating);
+		set_attendance(attendance);
+		cout << "SConstructor:\t" << this << endl;
+	}
+	~Student()
+	{
+		cout << "SDestructor:\t" << this << endl;
+	}
+
+	//				Methods:
+	void info()const override //переопределить
+	{
+		Human::info();
+		cout << speciality << " " << group << " " << rating << " " << attendance << endl;
+	}
+};
+
+class Teacher : public Human
+{
+	std::string speciality;
+	unsigned int experience;
+public:
+	const std::string& get_speciality()const
+	{
+		return speciality;
+	}
+	unsigned int get_experience()const
+	{
+		return experience;
+	}
+	void set_speciality(const std::string& speciality)
+	{
+		this->speciality = speciality;
+	}
+	void set_experience(unsigned int experience)
+	{
+		this->experience = experience;
+	}
+
+	//					Constructors:
+	Teacher(HUMAN_TAKE_PARAMETERS, const std::string& speciality, unsigned int experience) :
+		Human(HUMAN_GIVE_PARAMETERS)
+	{
+		set_speciality(speciality);
+		set_experience(experience);
+		cout << "TConstructor:\t" << this << endl;
+	}
+	~Teacher()
+	{
+		cout << "TDestructor:\t" << this << endl;
+	}
+
+	//					Methods:
+	void info()const override
+	{
+		Human::info();
+		cout << speciality << " " << experience << " years" << endl;
+	}
+};
+
+#define GRADUATE_TAKE_PARAMETERS const string& thesis_topic,const unsigned int grade
+#define GRADUATE_GIVE_PARAMETERS thesis_topic,grede
+class Graduate : public Student
+{
+	std::string thesis_topic;
+	unsigned int grade;
+
+public:
+	const std::string& get_thesis_topic()
+	{
+		return thesis_topic;
+	}
+	const unsigned int get_grade()
+	{
+		return grade;
+	}
+	void set_thesis_topic(const std::string thesis_topic)
+	{
+		this->thesis_topic = thesis_topic;
+	}
+	void set_grade(unsigned int grade)
+	{
+		this->grade = grade;
+	}
+
+	Graduate(HUMAN_TAKE_PARAMETERS,STUDENT_TAKE_PARAMETERS,GRADUATE_TAKE_PARAMETERS):
+		Student(HUMAN_GIVE_PARAMETERS, STUDENT_GIVE_PARAMETERS)
+	{
+		set_thesis_topic(thesis_topic);
+		set_grade(grade);
+		cout << "GConstructor:\t" << this << endl;
+	}
+	~Graduate()
+	{
+		cout << "GDestructor:\t" << this << endl;
+	}
+
+	void info()const override
+	{
+		Student::info();
+		cout << "thesis topic: " << thesis_topic << "\ngrade: " << grade << endl;
+	}
+};
+
+std::ostream& operator<<(std::ostream& os, const Human& obj)
+{
+	obj.info();
+	return os;
+}
+
+//#define INHERITANCE_CHECK
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+#ifdef INHERITANCE_CHECK
+	Human human("Vercetty", "Tommy", 30);
+	human.info();
+
+	Student student("Pinkman", "Jessie", 22, "Chemistry", "WW_220", 70, 97);
+	student.info();
+
+	Teacher teacher("White", "Walter", 50, "Chemistry", 25);
+	teacher.info();
+#endif // INHERITANCE_CHECK
+
+	/*
+	Plymorphism (Poly - много, Morphis - форма)
+	--------------------
+	AdHoc Polymorphism - Статический полиморфизм.
+	--------------------
+	Inclusion Polymorphism
+	1. Base Class Pointer - Generalization;
+	2. virtual functions;
+		VFPTR - Virtual Functions Pointers (Таблица указателей на виртуальные функции)
+	*/
+
+	//	Generalization:
+	Human* group[] =
+	{
+		new Student("Pinkman", "Jessie", 22, "Chemistry", "WW_220", 70, 97),
+		new Teacher("White", "Walter", 50, "Chemistry", 25),
+		new Student("Vercetty", "Tommy", 30, "Theft", "Vice", 97, 98),
+		new Graduate("Vasilii","Vasiliavich",32,"Physics","group",25,55,"topic",99)
+	};
+	cout << delimiter << endl;
+	for (int i = 0; i < sizeof(group) / sizeof(group[0]); i++)
+	{
+		//group[i]->info();
+		cout << *group[i] << endl;
+		cout << delimiter << endl;
+	}
+
+	for (int i = 0; i < sizeof(group) / sizeof(group[0]); i++)
+	{
+		delete group[i];
+	}
+	
+}
---
+++ Inheritance/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+
+
+    system("pause");
+
+    return 0;
+}
